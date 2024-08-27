30d10c71d8bb91443916c4e90eea8a902e412587
GiorgettaR <giorgetta.roberto@gmail.com>
2024-08-27 18:20:00 +0200

    ADD: bootstrap / projects json / project preview scaffholding

--- src/components/ProjectPreview.vue
+++ src/components/ProjectPreview.vue
@@ -1,8 +1,37 @@
 <template>
-  
+  <div class="projectPreview d-flex w-50 h-50">
+    <h1>{{ project.title }}</h1>
+    <div class="imgBox">
+        <img :src="getImgPath(project.imagePreview)" alt="">
+    </div>
+    <!-- <a :href="project.gitRepository">GitRepository</a> -->
+    <p class="link" @click="openGitRepository()">Git Repository</p>
+</div>
 </template>
 
 <script>
+export default {
+    props: {
+      project: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+        return {
+        }
+    },
+    methods:{
+        openGitRepository(){
+            window.open('https://github.com/giorgettaR/vue-boolzapp')
+        },
+        getImgPath(imgFile) {
+                let imgPath = '/img/' + imgFile
+                return imgPath;
+        },
+    }
+}
+
 
 </script>
 
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,10 +1,31 @@
 <template>
+    <div class="pageProjects wrap d-flex flex-wrap">
+            <ProjectPreview v-for="project in projects" :project="project"/>
+    </div>
     <h1>Projects</h1>
-  
 </template>
 
 <script>
+    import { data } from '../data.js';
+    import projects from '../projects.json'
+    import ProjectPreview from '../components/ProjectPreview.vue';
 
+    export default {
+        data() {
+            return {
+                data,
+                projects: projects.projects,
+            }
+        },
+        components: {
+            ProjectPreview
+        },
+        methods: {
+            mounted() {
+                console.log(projects)
+            },
+        }
+    }
 </script>
 
 <style lang ="scss">
