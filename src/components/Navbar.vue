f0cde5366d78c901fde21e0e703557760e507ba1
GiorgettaR <giorgetta.roberto@gmail.com>
2024-09-12 17:21:38 +0200

    FIX navbar animations

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,33 @@
 <template>
   <div class="navBar p-2 d-flex align-items-center justify-content-around">
-    <RouterLink :to="{ name: 'home'}">Home <span class="behind">Home</span></RouterLink>
-    
-    <RouterLink :to="{ name: 'projects'}" >{{ data.language == 'Italian' ? 'Progetti' : 'Projects'}}</RouterLink>
-    <RouterLink :to="{ name: 'mystory'}">{{ data.language == 'Italian' ? 'La Mia Storia' : 'Mystory'}}</RouterLink>
-    <RouterLink :to="{ name: 'contacts'}">{{ data.language == 'Italian' ? 'Contatti' : 'Contacts'}}</RouterLink>
+    <RouterLink
+      id="home"
+      v-on:mouseover="hoverLink('home')"
+      v-on:mouseleave="leaveLink('home')"
+      :to="{ name: 'home'}">
+      Home
+    </RouterLink>
+    <RouterLink
+      id="projects"
+      v-on:mouseover="hoverLink('projects')"
+      v-on:mouseleave="leaveLink('projects')"
+      :to="{ name: 'projects'}" >
+     {{ data.language == 'Italian' ? 'Progetti' : 'Projects'}}
+    </RouterLink>
+    <RouterLink
+      id="mystory"
+      v-on:mouseover="hoverLink('mystory')"
+      v-on:mouseleave="leaveLink('mystory')"
+      :to="{ name: 'mystory'}">
+      {{ data.language == 'Italian' ? 'La Mia Storia' : 'Mystory'}}
+    </RouterLink>
+    <RouterLink
+      id="contacts"
+      v-on:mouseover="hoverLink('contacts')"
+      v-on:mouseleave="leaveLink('contacts')"
+      :to="{ name: 'contacts'}">
+      {{ data.language == 'Italian' ? 'Contatti' : 'Contacts'}}
+    </RouterLink>
     <button class="flag"
       v-on:mouseover="hoverFlag()"
       v-on:mouseleave="leaveFlag()"
@@ -17,9 +40,7 @@
 <script>
 import { data } from '../data.js';
 import { gsap } from 'gsap'
-import SplitText from 'gsap-trial/SplitText';
 
-gsap.registerPlugin(SplitText)
 
 export default {
 
@@ -35,6 +56,20 @@ export default {
     getImgPath(imgFile) {
       let imgPath = '/img/' + imgFile
       return imgPath;
+    },
+    hoverLink(linkId) {
+      gsap.to(`#${linkId}`, {
+      scale: 1.3,
+      color: 'rgb(39, 111, 204)',
+      duration: 0.3
+      })
+    },
+    leaveLink(linkId) {
+      gsap.to(`#${linkId}`, {
+      scale: 1,
+      color: 'white',
+      duration: 0.3
+      })
     },
     hoverFlag() {
       gsap.to('.flag', {
